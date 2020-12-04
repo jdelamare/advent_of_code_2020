44fefb9a10808f723f9efc460823b877889302a8
dejef <dejef@pdx.edu>
2020-12-03 22:49:55 -0800

    Made my graph of trees
    I'm going to get some rest before dealing with indexing code. Amazing
    how good people are at these problems.
    
    I had dreams of more closures and functional code. Doesn't seem to be
    panning out.

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let s = fs::read_to_string("input.txt")
         .expect("Something went wrong reading the file"); 
     
-    let vv = produce_graph(s);
+    let vv = produce_graph(s, 1, 3);
 
     let soln = find_trees(vv);
 
@@ -12,15 +12,45 @@ fn main() {
 }
 
 
-fn produce_graph(s: String) -> Vec<Vec<bool>> {
+fn produce_graph(s: String, down: usize, right: usize) -> Vec<Vec<bool>> {
     // nothing clever, just put all the trees in a vec of vec.
     // expecting this to fluctuate in size for part 2 so vec over array
+    let mut vvc: Vec<Vec<char>> = vec![vec![]];
+    let mut vvb: Vec<Vec<bool>> = vec![vec![]];
 
-    vec![vec![true]]
+    // not space efficient, makes a square
+    let region_width = s.lines().nth(0).unwrap().chars().count();
+    let region_height = s.lines().count();
+    println!("rh{:?}", region_height);
+    println!("rw{:?}", region_width);
+
+    // if the amount we go down is less than the width than the region is already big enough
+    // else the worst case we go right on every single row, make vec that big
+    let total_width = match down * region_height < region_width {
+        true => 1,
+        false => (right as f32 * region_height as f32 / region_width as f32).ceil() as usize
+    };
+    println!("w: {}", total_width);
+    s.lines().for_each(|l| vvc.push(l.chars().collect()));
+
+    for vc in vvc {
+        let mut vb = vec![];
+        vc.iter().for_each(|c| if *c == '.' { vb.push(false); } else { vb.push(true); } );
+        for _ in 0..total_width {
+            vvb.push(vb.as_slice().repeat(total_width));
+        }
+    }
+
+    vvb
 }
 
-fn find_trees(vv: Vec<Vec<bool>>) -> u16 {
+fn find_trees(vv: Vec<Vec<bool>>, right: usize, down: usize) -> u16 {
     // we never wrap around the edge in this method, so just index magic
-
+    let mut trees = 0;
+    let mut down_posn = 0;
+    let mut right_posn = 0;
+    for v in vv {
+        
+    }
     0
 }
